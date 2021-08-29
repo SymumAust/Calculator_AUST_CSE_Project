fecc49683f2c296d5b0c3b5f8638a223e4b4fea4
Symum <symum952000@gmail.com>
2021-08-30 01:12:37 +0600

    First Commit

---
+++ Calculator/src/calculator/operations.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author admin
+ */
+public interface operations {
+    
+    void plus(double a, double b);
+    void minus(double a, double b);
+    void multiplication(double a, double b);
+    void division(double a, double b);
+    
+}
